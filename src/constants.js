b652ef28e1d68323abea9b3d1f21ec90157249b8
IGOR <kondrahov.2000@mail.ru>
2021-07-31 20:28:01 +0300

    Set constants to const

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-export let redColorTime = 10;
-export let yellowColorTime = 3;
-export let greenColorTime = 15;
-export let up = 'up';
-export let down = 'down';
-export let greenColor = 'rgb(39, 232, 51)'
+export const redColorTime = 10;
+export const yellowColorTime = 3;
+export const greenColorTime = 15;
+export const up = 'up';
+export const down = 'down';
+export const greenColor = 'rgb(39, 232, 51)'
